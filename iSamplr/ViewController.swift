24adca97f81aff0ffbffb60c2ddaf2a2e8c49f74
Hun Jae Lee <hunjael@gmail.com>
2016-04-03 02:00:50 -0400

    Initial Commit

---
+++ iSamplr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSamplr
+//
+//  Created by Hun Jae Lee on 4/3/16.
+//  Copyright © 2016 Hun Jae Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
